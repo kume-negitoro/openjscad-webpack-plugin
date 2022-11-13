685dfd7d2251c9727353199c476f46051c2e750e
kume-negitoro <kume.negitoro@gmail.com>
2022-11-13 14:02:46 +0900

    fix hook to support separate entries

--- index.js
+++ index.js
@@ -19,28 +19,31 @@ class OpenJSCADWebpackPlugin {
     }
 
     async hook(compilation){
-        const { options: { output: { path: dir, filename } } } = compilation
-        const target = path.resolve(dir, filename)
-        const source = await fs.readFile(target, 'utf-8')
-
+        const { options: { output: { path: dir } } } = compilation
         const { addMetaData, format, params } = this.options
 
         await Promise.all(
-            format.map(format => {
-                const name = path.basename(filename, '.js')
-                const outpath = path.resolve(dir, `${name}.${format}`)
-
-                return generateOutputData(source, params, {
-                    outputFile: outpath,
-                    outputFormat: format,
-                    inputFile: target,
-                    inputFormat: 'js',
-                    version,
-                    addMetaData,
+            Object.keys(compilation.assets)
+                .flatMap(async filename => {
+                    const target = path.resolve(dir, filename)
+                    const source = await fs.readFile(target, 'utf-8')
+                    return format.map(format => {
+                        const name = path.basename(filename, '.js')
+                        const outpath = path.resolve(dir, `${name}.${format}`)
+        
+                        return generateOutputData(source, params, {
+                            outputFile: outpath,
+                            outputFormat: format,
+                            inputFile: target,
+                            inputFormat: 'js',
+                            version,
+                            addMetaData,
+                        })
+                            .then(data => fs.writeFile(outpath, data.asBuffer()))
+                            .catch(error => console.error(error))
+                    })
+                    
                 })
-                    .then(data => fs.writeFile(outpath, data.asBuffer()))
-                    .catch(error => console.error(error))
-            })
         )
         
         delete require.cache[target]
